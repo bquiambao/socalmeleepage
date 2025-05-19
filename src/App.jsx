87e32cdb3546fdc380a1e9b360b8ed0788444f34
bquiambao <imbryanquiambao@gmail.com>
2025-05-18 20:03:36 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Static Site</h1>
+      <p>This is built with React!</p>
+    </div>
+  );
+}
+
+export default App;
+
